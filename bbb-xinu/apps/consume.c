2ce7d074bd20fe9486215932ca567a2761111ad7
sirdas <sirdas@indiana.edu>
2015-09-21 20:31:20 -0400

    Consumer part of the producer consumer problem

--- bbb-xinu/apps/consume.c
+++ bbb-xinu/apps/consume.c
@@ -5,4 +5,8 @@ void consumer(int count)
   //Code to consume values of global variable 'n' until the value
   // of n is less than or equal to count
   //print consumed value e.g. consumed : 8
+    while (n <= count) {
+	printf("consumed : %d", n);
+	n--;
+    }
 }
