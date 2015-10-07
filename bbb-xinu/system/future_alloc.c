e655c9707c5f48abba4602414adfb31ad4272e84
sirdas <sirdas@indiana.edu>
2015-10-07 10:07:16 -0400

    Basic Skeleton for Futures

---
+++ bbb-xinu/system/future_alloc.c
@@ -0,0 +1,13 @@
+#include <xinu.h>
+#include <future.h>
+
+future* future_alloc(int future_flag)
+{
+    future *f;
+    f = (future *)getmem(sizeof(future));
+    if(f == NULL) {
+	return NULL;
+    }
+    f->flag = future_flag;
+    return f;
+}
