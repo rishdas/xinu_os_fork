a67f6ac5ef582e5fbb8419e8f544c080904ce9c4
Lucas Brasilino <lbrasili@github.iu.edu>
2015-11-27 12:51:14 -0500

    resolving conflics from previous merge

--- bbb-xinu/apps/consume.c
+++ bbb-xinu/apps/consume.c
@@ -1,23 +1,5 @@
-<<<<<<< .merge_file_5hPPeD
-#include <prodcons.h>
-
-void consumer(int count)
-{
-    //Code to consume values of global variable 'n' until the value
-    // of n is less than or equal to count
-    //print consumed value e.g. consumed : 8
-    while (n <= count) {
-	wait(producedsem);
-	printf("consumed : %d\n", n);
-	signal(consumedsem);
-    }
-    if (n == count) semdelete (producedsem);
-}
-=======
 #include <xinu.h>
 #include <prodcons.h>
 
 void consumer(int count) {
 }
-
->>>>>>> .merge_file_l58XKC
--- bbb-xinu/apps/produce.c
+++ bbb-xinu/apps/produce.c
@@ -1,27 +1,5 @@
-<<<<<<< .merge_file_1YbzEK
-#include <prodcons.h>
-
-void producer(int count)
-{
-    //Code to produce values less than equal to count, 
-    //produced value should get assigned to global variable 'n'.
-    //print produced value e.g. produced : 8
-
-    while (n < count) {
-	wait(consumedsem);
-	n++;
-	printf ("produced: %d\n",n);
-	signal(producedsem);
-    }
-    if (n == count) semdelete (consumedsem);
-}
-=======
 #include <xinu.h>
 #include <prodcons.h>
 
 void producer(int count) {
 }
-
-
-
->>>>>>> .merge_file_0lELCL
